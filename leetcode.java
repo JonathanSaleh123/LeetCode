38418b19a3e7d1e23495c267fd30bc781bf0b2b5
JonathanSaleh123 <jonathan160720032@gmail.com>
2022-01-11 18:43:52 -0800

    Day 2

--- leetcode.java
+++ leetcode.java
@@ -101,4 +101,108 @@ class Solution5{
 //This solution is to find the halfway point and see if it is good backwards.
 
 
-//N-ary Tree Traversal
+//N-ary Tree Traversal Postorder (Bottom to top)
+class Solution6{
+    public List<Integer> postorder(Node root){
+        LinkedList<Node> stack = new LinkedList<>();
+        LinkedList<Integer> output_arr = new LinkedList<>();
+
+        if (root == null){
+            return output_arr;
+        }
+        stack.add(root);
+        while (!stack.isEmpty()){
+            Node node = stack.pollLast();
+            output_arr.addFirst(node.val);
+            for (Node child : node.children){
+                stack.add(child);
+            }
+        }
+        return output_arr;
+    }
+}
+//N-ary Tree Traversal Inorder (Left subtree to right)
+class Solution7{
+    public List<Integer> inorderTraversal(TreeNode root){
+        Stack<TreeNode> stack = new Stack<>();
+        List<Integer> output_arr = new ArrayList<>();
+
+        if(root == null){
+            return output_arr;
+        }
+
+        TreeNode current = root;
+        while (current != null || stack.isEmpty()){
+            while(current!= null){
+                stack.push(current);
+                current = current.left;
+            }
+            current = stack.pop();
+            output_arr.add(current.val);
+            current = current.right;
+        }
+        return output_arr;
+    }
+}
+
+//N-ary Tree Traersal Pre-order (Top to bottom)
+class Solution8{
+    public List<Integer> preorder(Node root){
+        Stack<Node> stack = new Stack<>();
+        LinkedList<Integer> output_arr = new LinkedList<>();
+
+        if (root==null){
+            return output_arr;
+        }
+        stack.add(root);
+        while (!stack.isEmpty()){
+            Node node = stack.pollLast();
+            output_arr.add(node.val);
+            Collections.reverse(node.children); 
+            //Reverses the linked list as it usually goes from left to right
+            for (Node child : node.children){
+                stack.add(child);
+            }
+        }
+        return output_arr;
+    }
+}
+
+
+//Merge Two Binary Trees
+
+class Solution9{
+    public TreeNode mergeTrees(TreeNode t1, TreeNode t2){
+        if (t1 == null){
+            return t2;
+        }
+        if (t2 == null){
+            return t1;
+        }
+        t1.val += t2.val;
+        t1.left = mergeTrees(t1.left, t2.left);
+        t1.right = mergeTrees(t1.right, t2.right);
+        return t1;
+    }
+}
+
+//Invert Binary Tree
+//First Glance Solution make a stack and make a new tree
+class Solution10{
+    public TreeNode invertTree(TreeNode root){
+        if (root == null){
+            return root;
+        }
+
+        TreeNode left = invertTree(root.left);
+        TreeNode right = invertTree(root.right);
+
+        //swap
+        root.right = left;
+        root.left = right;
+
+        return root;
+    }
+}
+
+//
